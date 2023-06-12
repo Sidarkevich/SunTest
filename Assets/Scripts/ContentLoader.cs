b5f0a44384ce469b324db94668eecad628af40a2
Yaraslau <ysidarkevich@gmail.com>
2023-06-12 14:06:43 +0300

    unitask implemented

--- Assets/Scripts/ContentLoader.cs
+++ Assets/Scripts/ContentLoader.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Events;
+using Cysharp.Threading.Tasks;
 
 public class ContentLoader : MonoBehaviour
 {
@@ -14,9 +15,57 @@ public class ContentLoader : MonoBehaviour
     private List<Sprite> _content;
     public List<Sprite> Downloaded => _content;
 
-    public void LoadData()
+    private void Start()
     {
-        StartCoroutine(LoadTextureFromServer());
+        LoadAsync();
+        //StartCoroutine(LoadTextureFromServer());
+    }
+
+    private async void LoadAsync()
+    {
+        Debug.Log("Start downloading..");
+
+        var errorsCount = 0;
+        var nextId = 1;
+
+        while (errorsCount < _allowableErrorsCount)
+        {
+            try 
+            {
+                var sprite = await LoadSpriteAsync($"http://data.ikppbb.com/test-task-unity-data/pics/{nextId}.jpg");
+                _downloaded.Add(nextId, sprite);
+                Debug.Log("Downloaded: " + nextId);
+            }
+            catch
+            {
+                Debug.Log("Failed while downloading " + nextId);
+                errorsCount++;
+            }
+
+            nextId++;
+        }
+
+        _content = new List<Sprite>(_downloaded.Values);
+        Debug.Log("Download finished! Result: " + _content.Count);
+
+        ContentLoadedEvent?.Invoke(_content);
+    }
+
+    private async UniTask<Sprite> LoadSpriteAsync(string url)
+    {
+        using var request = UnityWebRequestTexture.GetTexture(url);
+
+        await request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            var texture = DownloadHandlerTexture.GetContent(request);
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        }
+        else
+        {
+            return null;
+        }
     }
 
     IEnumerator LoadTextureFromServer()
